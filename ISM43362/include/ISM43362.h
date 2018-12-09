083903b38b15b98b1c1cfef4748c23756ee6bcb2
Dan Shemesh <dan@jumper.io>
2018-12-09 11:42:39 +0200

    ISM43362 update

--- ISM43362/include/ISM43362.h
+++ ISM43362/include/ISM43362.h
@@ -14,6 +14,12 @@ struct SslData {
     std::string client_key {};
 };
 
+static const std::string NEW_LINE = "\r\n";
+static const std::string PROMPT = NEW_LINE + "> ";
+static const std::string OK = NEW_LINE + "OK";
+static const std::string ERROR = NEW_LINE + "ERROR";
+static const std::string START_CONN = "\x15\x15" + PROMPT;
+
 class ISM43362 : public ExternalPeripheral {
   public:
     ISM43362();
@@ -24,33 +30,47 @@ class ISM43362 : public ExternalPeripheral {
     ISM43362& operator=(const ISM43362&) = delete;
 
   private:
+    void MainLoop();
     void Reset();
     void HandleRead();
     void HandleWrite();
-    void ResetWifi();
+    void Start();
     void OnPinChangeLevelEvent(std::vector<WireLogicLevelEvent>& notifications, std::string pin_name);
-
-    std::string buffer_{}; 
     void SpiWrite(const std::string& command);
     void SpiWrite(uint8_t* buf, int size);
     void OnRecv();
     void OnSend(AtCommand& at_command, uint8_t* buf, int size);
     void OnDNSResolve(AtCommand& at_command, uint8_t* buf, int size);
     void OnSslCert(AtCommand& at_command, uint8_t* buf, int size);
-    // void SendTxEvent();
     void HandleAtCommand(uint8_t* buf, int size);
+    void HandleAtCommandP6(bool client_enable);
     void SpiWriteCommandAndOk(std::string data);
+    void SpiDataPhase(std::string data);
+    void SpiCommandPhase();
+    void CmdOrDataReadyPinSignal(transition_type_t type);
+    void StartCommandPhase() { CmdOrDataReadyPinSignal(RISING); }
+    void EndCommandPhase() { CmdOrDataReadyPinSignal(FALLING); }
+    void StartDataPhase() { CmdOrDataReadyPinSignal(RISING); }
+    void EndDataPhase() { CmdOrDataReadyPinSignal(FALLING); }
+    void WifiSendOk(transition_type_t type);
+    void WarpDataToSend(std::string& data) { data = NEW_LINE + data + OK + PROMPT; }
+    std::string GetNetworkSetting();
 
     iSpiSlaveV2* spi_slave_ {};
-
-    int reset_done_ {};
+    bool reset_mode_ {};
+    bool should_stop_ {};
+    bool is_ssl_ {};
+    std::string buffer_{};
     std::string ap_ {};
     std::string password_ {};
-    bool should_stop_ {false};
+    std::string security_type_ {};
+    std::string dhcp_ {};
+    std::string connected_ {"0"};
     int current_id_ {-1};
-    int current_ssl_certificate {-1};
+    int current_ssl_certificate_ {-1};
     std::vector<SocketData> id_2_socket_data_;
     std::vector<SslData> ssl_certificates_;
+    uint32_t bytes_to_read_ {};
 };
 
 DLL_EXPORT ExternalPeripheral *PeripheralFactory() {
--- ISM43362/src/ISM43362.cpp
+++ ISM43362/src/ISM43362.cpp
@@ -1,13 +1,8 @@
 #include "ISM43362.h"
 #include <netinet/in.h>
 #include <iostream>
-#include <cstring>
 #include <arpa/inet.h>
-
-#define SPI_WRITE_OK    SpiWrite("\r\nOK\r\n> ")
-#define SPI_WRITE_ERROR    SpiWrite("\r\nERROR")
-
-#define SPI_WRITE_OK_INT(data)    SpiWrite(std::string("\r\n") + std::to_string(data) + "\r\nOK\r\n> ")
+#include <string>
 
 using namespace std;
 
@@ -33,102 +28,42 @@ struct AtCommand {
 
 
 ISM43362::ISM43362() : id_2_socket_data_(10), ssl_certificates_(3) {
-    SpiSlaveConfig spi_config;
+    SpiSlaveConfig spi_config {};
 
     spi_config.mosi_pin_number = GetPinNumber("si");
     spi_config.miso_pin_number = GetPinNumber("so");
     spi_config.ss_pin_number = GetPinNumber("cs");
     spi_config.sclk_pin_number = GetPinNumber("sck");
     spi_config.supported_spi_modes = SPI_MODE_CPOL0_CPHA0;
-    spi_config.max_frequency = 10000000;
+    spi_config.max_frequency = 20000000;  // SPI Slave Clock rate: 20MHz max
     spi_config.bit_order = MSB_FIRST;
 
     spi_slave_ = CreateSpiSlave(spi_config);
 
-    SetPinChangeLevelEventCallback(GetPinNumber("WAKEUP"), std::bind(&ISM43362::OnPinChangeLevelEvent,this,std::placeholders::_1, "WAKEUP"));
-    SetPinChangeLevelEventCallback(GetPinNumber("RESET"), std::bind(&ISM43362::OnPinChangeLevelEvent,this,std::placeholders::_1, "RESET"));
-    SetPinChangeLevelEventCallback(GetPinNumber("EXTI1"), std::bind(&ISM43362::OnPinChangeLevelEvent,this,std::placeholders::_1, "EXTI1"));
-    SetPinChangeLevelEventCallback(GetPinNumber("UART3_TX"), std::bind(&ISM43362::OnPinChangeLevelEvent,this,std::placeholders::_1,"UART3_TX"));
-    SetPinChangeLevelEventCallback(GetPinNumber("UART3_RX"), std::bind(&ISM43362::OnPinChangeLevelEvent,this,std::placeholders::_1,"UART3_RX"));
-
-    Reset();
-}
-
-void ISM43362::SpiWriteCommandAndOk(std::string data) {
-    SpiWrite(std::string("\r\n") + data + "\r\nOK\r\n> ");
+//    SetPinChangeLevelEventCallback(GetPinNumber("WAKEUP"), std::bind(&ISM43362::OnPinChangeLevelEvent,this,std::placeholders::_1, "WAKEUP"));
+//    SetPinChangeLevelEventCallback(GetPinNumber("RESET"), std::bind(&ISM43362::OnPinChangeLevelEvent,this,std::placeholders::_1, "RESET"));
+//    SetPinChangeLevelEventCallback(GetPinNumber("EXTI1"), std::bind(&ISM43362::OnPinChangeLevelEvent,this,std::placeholders::_1, "CMD_DATA_RDY"));
+//    SetPinChangeLevelEventCallback(GetPinNumber("UART3_TX"), std::bind(&ISM43362::OnPinChangeLevelEvent,this,std::placeholders::_1,"UART3_TX"));
+//    SetPinChangeLevelEventCallback(GetPinNumber("UART3_RX"), std::bind(&ISM43362::OnPinChangeLevelEvent,this,std::placeholders::_1,"UART3_RX"));
+//
+//    Reset();
 }
 
 void ISM43362::OnPinChangeLevelEvent(std::vector<WireLogicLevelEvent>& notifications, std::string pin_name) {
-    WireLogicLevelEvent notification = notifications[0];
-
-    // std::cout << "ISM43362::OnPinChangeLevelEvent: " << pin_name << " : " << notification.type << std::endl;
-
-    if (pin_name == "RESET") {
-        reset_done_ += notification.type == FALLING ? 1 : 0; 
-    }
-}
-
-void ISM43362::ResetWifi() {
-    uint8_t prompt[] = {0x15, 0x15, '\r','\n','>',' ', };
-    uint8_t opcode = 0;
-    
-    while (!should_stop_) {
-        if(reset_done_ == 0) {
-            continue;
-        }
-
-        spi_slave_->Transmit(nullptr, prompt, sizeof(prompt));
-        SetPinLevel(GetPinNumber("EXTI1"), 0);
-
-        spi_slave_->Transmit(nullptr, &opcode,1);
-        SetPinLevel(GetPinNumber("EXTI1"), 1); //ready to receive
-
-        return;
-    }
-}
-
-void ISM43362::SpiWrite(const std::string& data) {
-//    std::cout << "SpiWrite: " << data << std::endl;
-    SpiWrite((uint8_t *)data.c_str(), data.size());
-}
-
-void ISM43362::SpiWrite(uint8_t* buf, int size) {
-//    std::cout << "SpiWrite: " << buf << std::endl;
-    // aligment
-    if( size % 2 == 1) {
-        buf[size] = 21;
-        size++;
-    }
-
-    SetPinLevel(GetPinNumber("EXTI1"), 1);
-    spi_slave_->Transmit(nullptr, buf, size);
-    SetPinLevel(GetPinNumber("EXTI1"), 0);
+    //WireLogicLevelEvent notification = notifications[0];
+    //std::cout << "ISM43362::OnPinChangeLevelEvent: " << pin_name << " : " << notification.type << std::endl;
 }
 
 void ISM43362::OnRecv() {
     uint8_t rec_buf[4096] = {0};
-    memcpy(rec_buf, "\r\n", 2);
-    int size = TcpSocketRecv(id_2_socket_data_[current_id_].socket_fd, rec_buf + 2, 4086);
-
-    if (size == -1) {
-        SpiWriteCommandAndOk("");
-        return;
-    }
-    // else if (size < 0) {
-    //     SpiWriteCommandAndOk("");
-    //     return;
-    // }
-    // cout << std::string((char*)rec_buf + 2, size) << std::endl;
-
-    memcpy(rec_buf + 2 + size, "\r\nOK\r\n> ", 8);
-    
-    SpiWrite(rec_buf, size + 10);
+    int size = TcpSocketRecv(id_2_socket_data_[current_id_].socket_fd, rec_buf, bytes_to_read_);
+    (size == -1) ? SpiDataPhase("") : SpiDataPhase(std::string(rec_buf, rec_buf + size));
 }
 
 void ISM43362::OnSend(AtCommand& at_command, uint8_t* buf, int size) {
     int right = std::stoi(at_command.right_);  
     int sent_bytes = TcpSocketSend(id_2_socket_data_[current_id_].socket_fd, buf + size - right, right);
-    SPI_WRITE_OK_INT(sent_bytes);
+    SpiDataPhase(std::to_string(sent_bytes));
 }
 
 void ISM43362::OnDNSResolve(AtCommand& at_command, uint8_t* buf, int size) {
@@ -136,12 +71,12 @@ void ISM43362::OnDNSResolve(AtCommand& at_command, uint8_t* buf, int size) {
     struct in_addr ip;
     if (!DnsResolve(at_command.right_.c_str(), &ip)) {
 //        std::cout << "Error: hostname could not be resolved" << std::endl;
-        SPI_WRITE_ERROR;
+        SpiDataPhase(ERROR);
         return;
     }
 
     strcpy(ip_string, inet_ntoa(ip));
-    SpiWriteCommandAndOk(ip_string);
+    SpiDataPhase(ip_string);
 }
 
 vector<string> split(const string& str, const string& delim) {
@@ -180,119 +115,121 @@ void ISM43362::OnSslCert(AtCommand& at_command, uint8_t* buf, int size) {
         }
     }
 
-    SPI_WRITE_OK;
+    SpiDataPhase("");
 }
 
+/* https://www.inventeksys.com/iwin/at-cmds */
 void ISM43362::HandleAtCommand(uint8_t* buf, int size) {
     AtCommand at_command = AtCommand(buf, size);
 //    std::cout << "Command: " << at_command.full_command_ << std::endl;
 
-    if (at_command.left_ == "I?") {
-        SpiWriteCommandAndOk("ISM43362-M3G-L44-SPI,C3.5.2.5.STM,v3.5.2,v1.4.0.rc1,v8.2.1,120000000,Inventek eS-WiFi");
-    } else if (at_command.left_ == "CS") {
-        SPI_WRITE_OK_INT(0);
-    } else if (at_command.left_ == "C1") {
+    if (at_command.left_ == "I?") { /* Show Applications Information */
+        SpiDataPhase("ISM43362-M3G-L44-SPI,C3.5.2.5.STM,v3.5.2,v1.4.0.rc1,v8.2.1,120000000,Inventek eS-WiFi");
+    } else if (at_command.left_ == "C0") { /* Join a Network */
+        SpiDataPhase("");
+    } else if (at_command.left_ == "C1") { /* Set Network SSID */
         ap_ = at_command.right_;
-        SPI_WRITE_OK; // C1=hello
-    } else if (at_command.left_ == "C2") {
+        SpiDataPhase("");
+    } else if (at_command.left_ == "C2") { /* Set Network Passphrase */
         password_ = at_command.right_;
-        SPI_WRITE_OK; // C2=Bello123
-    } else if (at_command.left_ == "C3") {
-        SPI_WRITE_OK; // C3=3
-    } else if (at_command.left_ == "C0") {
-        SPI_WRITE_OK;
-    } else if (at_command.left_ == "C?") {
-        SpiWriteCommandAndOk(ap_ + "," + password_ + ",3,1,0,192.168.1.12,255.255.255.0,192.168.1.1,192.168.1.1,192.168.1.1,3,0,0,US,1");
-    } else if (at_command.left_ == "D0") {
+        SpiDataPhase("");
+    } else if (at_command.left_ == "C3") { /* Set Network Security Type */
+        security_type_ = at_command.right_;
+        SpiDataPhase("");
+    } else if (at_command.left_ == "C4") { /* Set Network DHCP */
+        dhcp_ = at_command.right_;
+        SpiDataPhase("");
+    } else if (at_command.left_ == "CD") { /* Disconnect from Network */
+        id_2_socket_data_[current_id_] = SocketData();
+        connected_ = "0";
+        SpiDataPhase("");
+    } else if (at_command.left_ == "CR") { /* Get RSSI of Associated Access Point */
+        SpiDataPhase("-40"); // dummy value
+    } else if (at_command.left_ == "CS") { /* Get Connection Status */
+        SpiDataPhase(connected_);
+    } else if (at_command.left_ == "C?") { /* Show Network Settings */
+        SpiDataPhase(GetNetworkSetting());
+    } else if (at_command.left_ == "D0") { /* DNS Lookup */
         OnDNSResolve(at_command, buf, size);
-    } else if (at_command.left_ == "PF") {
-        current_ssl_certificate = stoi(split(at_command.right_, ",")[1]) - 1;
-        SPI_WRITE_OK; // "PF=0,3"
-    } else if (at_command.left_ == "PG") {
-        OnSslCert(at_command, buf, size);//"PG=3,0,1733"
-    } else if (at_command.left_ == "P0") {
+    } else if (at_command.left_ == "F0") { /* Scan for Network Access Points */
+        SpiDataPhase("#001,\"DUMMY\",08:86:3B:2B:7E:2E,-40,54.0,Infrastructure, WPA2 AES,2.4GHz,1"); // dummy value
+    } else if (at_command.left_ == "P0") { /* Set/Display Communication Socket */
         current_id_ = stoi(at_command.right_);
-        SPI_WRITE_OK;
-    } else if (at_command.left_ == "P3") {
+        SpiDataPhase("");
+    } else if (at_command.left_ == "P1") { /* Set Transport Protocol */
+        if (at_command.right_ != "0" && at_command.right_ != "3") /* TCP = 0, TCP-SSL = 3 */
+            throw std::logic_error("ISM43362::HandleAtCommand: Unimplemented Protocol (UDP)");
+        SpiDataPhase("");
+    } else if (at_command.left_ == "P2") { /* Set Transport Local Port Number */
+        SpiDataPhase("");
+    } else if (at_command.left_ == "P3") { /* Set Transport Remote Host IP Address */
         id_2_socket_data_[current_id_].addr.sin_family = AF_INET;
         inet_pton(AF_INET, at_command.right_.c_str(), &(id_2_socket_data_[current_id_].addr.sin_addr));
-        SPI_WRITE_OK;
-    } else if (at_command.left_ == "P4") {
+        SpiDataPhase("");
+    } else if (at_command.left_ == "P4") { /* Set Transport Remote Port Number */
         id_2_socket_data_[current_id_].addr.sin_port = htons(stoi(at_command.right_));
-        SPI_WRITE_OK;
-    } else if (at_command.left_ == "P6") {
-        bool result;
-        if (at_command.right_[0] == '0') {
-            result = SocketClose(id_2_socket_data_[current_id_].socket_fd);
-        } else {
-            SocketData &socket_data = id_2_socket_data_[current_id_];
-            id_2_socket_data_[current_id_].socket_fd = TcpSocketCreate();
-            SetSocketRecvTimeout(id_2_socket_data_[current_id_].socket_fd, 5, 0);
-            SetSocketSendTimeout(id_2_socket_data_[current_id_].socket_fd, 5, 0);
-            bool is_ssl = (current_ssl_certificate != -1);
-            if (is_ssl) {
-                result = TcpSocketConnect(
-                        socket_data.socket_fd,
-                        &socket_data.addr,
-                        true,
-                        ssl_certificates_[current_ssl_certificate].ca_cert,
-                        ssl_certificates_[current_ssl_certificate].client_cert,
-                        ssl_certificates_[current_ssl_certificate].client_key);
-            } else {
-                result = TcpSocketConnect(socket_data.socket_fd, &socket_data.addr, false, "", "", "");
-            }
-        }
-        if (result == 0) {
-            SPI_WRITE_OK;
-        } else {
-            SPI_WRITE_ERROR;
-        }
+        SpiDataPhase("");
+    } else if (at_command.left_ == "P6") { /* Stop/Start Transport Client */
+        bool client_enable = at_command.right_[0] == '1';
+        HandleAtCommandP6(client_enable);
+    } else if (at_command.left_ == "P9") { /* SSL Certification Verification Level */
+        is_ssl_ = at_command.right_ == "2";
+        SpiDataPhase("");
+    } else if (at_command.left_ == "PF") { /* Select Active Certificate Set */
+        current_ssl_certificate_ = stoi(split(at_command.right_, ",")[1]) - 1;
+        SpiDataPhase("");
+    } else if (at_command.left_ == "PG") { /* Program CA, Certificate or key */
+        OnSslCert(at_command, buf, size); //"PG=3,0,1733"
+    } else if (at_command.left_ == "R0") {
+        OnRecv();
+    } else if (at_command.left_ == "R1") { /* Set Read Transport Packet Size (bytes) */
+        bytes_to_read_ = std::stoi(at_command.right_);
+        SpiDataPhase("");
+    } else if (at_command.left_ == "R2") { /* Set Read Transport Timeout (ms) */
+        int ms = stoi(at_command.right_);
+        SetSocketRecvTimeout(id_2_socket_data_[current_id_].socket_fd, ms  / 1000, (ms * 1000) % 1000000);
+        SpiDataPhase(""); // timeout == 1
     } else if (at_command.left_ == "S2") {
         int ms = stoi(at_command.right_);
         SetSocketSendTimeout(id_2_socket_data_[current_id_].socket_fd, ms  / 1000, (ms * 1000) % 1000000);
-        SPI_WRITE_OK; // timeout == 1
-    } else if (at_command.left_ == "S3") {
+        SpiDataPhase(""); // timeout == 1
+    } else if (at_command.left_ == "S3") { /* Write Transport Data */
         OnSend(at_command, buf, size);
-    } else if (at_command.left_ == "R2") {
-        int ms = stoi(at_command.right_);
-        SetSocketRecvTimeout(id_2_socket_data_[current_id_].socket_fd, ms  / 1000, (ms * 1000) % 1000000);
-        SPI_WRITE_OK; // timeout == 1
-    } else if (at_command.left_ == "R0") {
-        OnRecv();
+    } else if (at_command.left_ == "Z5") { /* Get MAC Address */
+        SpiDataPhase("");
     } else {
-        SPI_WRITE_OK;
+        std::cerr << "Unimplemented AT command: " << at_command.full_command_ << std::endl;
+        SpiDataPhase("");
     }
+}
 
-    // std::map<std::string, std::function<void(AtCommand&, uint8_t* , int)>> at_command_2_function_;
-
-    // at_command_2_function_["S3"] = std::bind([&]() { OnSend(at_command, buf, size);});
-    // at_command_2_function_["R0"] = std::bind([&]() {OnRecv(); }
-
-    // auto it = at_command_2_function_.find(at_command.left_);
-    // if(it == at_command_2_function_.end()){
-    //     SPI_WRITE_OK;
-    //     return;
-    // }
-
-    // it->second(at_command, buf, size);
+void ISM43362::HandleAtCommandP6(bool client_enable) {
+    bool result {};
+    if (!client_enable) {
+        result = SocketClose(id_2_socket_data_[current_id_].socket_fd);
+    } else {
+        SocketData &socket_data = id_2_socket_data_[current_id_];
+        id_2_socket_data_[current_id_].socket_fd = TcpSocketCreate();
+        SetSocketRecvTimeout(id_2_socket_data_[current_id_].socket_fd, 5, 0);
+        SetSocketSendTimeout(id_2_socket_data_[current_id_].socket_fd, 5, 0);
+        if (is_ssl_) {
+            result = TcpSocketConnect(
+                  socket_data.socket_fd,
+                  &socket_data.addr,
+                  true,
+                  ssl_certificates_[current_ssl_certificate_].ca_cert,
+                  ssl_certificates_[current_ssl_certificate_].client_cert,
+                  ssl_certificates_[current_ssl_certificate_].client_key);
+        } else {
+            result = TcpSocketConnect(socket_data.socket_fd, &socket_data.addr, false, "", "", "");
+        }
+    }
+    if (result == 0) connected_ = "1";
+    result == 0 ? SpiDataPhase("") : SpiDataPhase(ERROR);
 }
 
 void ISM43362::Main() {
-    uint8_t buffer[4096];
-
-    ResetWifi();
-
-    while (!should_stop_) {
-        int read = spi_slave_->Transmit(buffer, nullptr , 4096);
-
-        if (read) {
-            SetPinLevel(GetPinNumber("EXTI1"), 0);
-            HandleAtCommand(buffer, read );
-        }
-        else {
-            SetPinLevel(GetPinNumber("EXTI1"), 1); //ready to receive
-        }
-    }
+    Start();
 }
 
 void ISM43362::Stop() {
@@ -302,8 +239,62 @@ void ISM43362::Stop() {
 // Reset all registers of ISM43362
 // All regs reset values are zero except IODIR register, which should be 0xFF
 void ISM43362::Reset() {
-//    std::fill(registers_, registers_ + MCP_REGS, 0);
-//    registers_[IODIR_ADDR] = 0XFF;
+}
 
-//   SpiWriteGPIO(0); // For reset output pin levels
+void ISM43362::CmdOrDataReadyPinSignal(transition_type_t type) {
+    SetPinLevel(GetPinNumber("EXTI1"), type);
+}
+
+void ISM43362::Start() {
+    /* The eS-WiFi module after power up or reset will raise CMD/DATA READY pin to signal that the first Data
+     * Phase has started. In this mode, the SPI Host must fetch the cursor. The Host will initiate a SPI cycle
+     * (lower SSN) and clock out 0x0A (Line Feed) until the CMD/DATA READY pin lowers signaling the end of the
+     * Data Phase. The data received will be 0x0d (CR) 0x0A (LF) 0x3E (>) 0x20 (SP). */
+    SpiDataPhase(START_CONN);
+    while (!should_stop_)
+        SpiCommandPhase();
+}
+
+/* The Command Phase indicates the eS-WiFi module is ready to accept an IWIN AT Command. */
+void ISM43362::SpiCommandPhase() {
+    StartCommandPhase();
+    uint8_t buffer[4096];
+    int bytes_read = spi_slave_->Transmit(buffer, nullptr , 4096);
+    EndCommandPhase();
+    if (bytes_read > 0)
+        HandleAtCommand(buffer, bytes_read);
+}
+
+/* The Data Phase indicates the eS-WiFi module has data ready for the Host to read. */
+void ISM43362::SpiDataPhase(std::string data) {
+    if (data != START_CONN && data != ERROR) WarpDataToSend(data);
+    uint8_t nak = 0x15;
+    size_t size = data.size();
+    if (size % 2 == 1) data += nak;
+    uint8_t* buffer = (uint8_t *) data.c_str();
+    size_t sent_bytes = 0;
+
+    while (sent_bytes < size || should_stop_) {
+        StartDataPhase();
+        sent_bytes = spi_slave_->Transmit(nullptr, buffer, size);
+        EndDataPhase();
+    }
+
+    while (spi_slave_->Transmit(nullptr, &nak, 1) != 0) {};  // wait until ss is inactive (sent_bytes == 0)
+}
+
+std::string ISM43362::GetNetworkSetting() {
+    const std::string ip_version = "0";
+    const std::string ip_address = "192.168.1.8";
+    const std::string mask = "255.255.255.0";
+    const std::string gateway = "192.168.1.1";
+    const std::string dns = "192.168.1.1";
+    const std::string retries = "5";
+    const std::string auto_connect = "0";
+    const std::string authentication = "0";
+    const std::string country_code = "0";
+    const std::string status = "1";
+    return ap_ + "," + password_ + "," + security_type_ + "," + dhcp_ + "," + ip_version + "," + ip_address + "," +
+          mask + "," + gateway + "," + dns + "," + dns + "," + retries + "," + auto_connect + "," + authentication +
+          "," + country_code + "," + status;
 }
